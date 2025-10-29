a09412582da87b44aaea7803b2f4954858a980f4
AL - Muiayed AL - Ansari <creeew89@gmail.com>
2025-10-29 15:13:31 +0400

    #TRA-5941: OOP > Shop > SmartPhone
    Simple POJO capturing a handset’s main-sensor MP, optical-zoom factor, and pro-mode flag.

---
+++ SmartPhone.java
@@ -0,0 +1,14 @@
+package Vibe;
+
+public class SmartPhone {
+    private int mainSensorMegapixels;
+    private double opticalZoomLevel;
+    private boolean proModeFeatures;
+
+    @Override
+    public String toString() {
+        return "SmartPhone{" +
+                "mainSensorMegapixels=" + mainSensorMegapixels +
+                '}';
+    }
+}
