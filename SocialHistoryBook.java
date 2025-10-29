6ecccd4a6a646723976bc019078e3917cf19b41c
AL - Muiayed AL - Ansari <creeew89@gmail.com>
2025-10-29 15:15:22 +0400

    #TRA-5942: OOP > Shop > SocialHistoryBook
    Lightweight record of a social-history volume’s target population and cultural artifacts covered.

---
+++ SocialHistoryBook.java
@@ -0,0 +1,13 @@
+package Vibe;
+
+public class SocialHistoryBook {
+    private String focusPopulation;
+    private String culturalArtifactsDiscussed;
+
+    @Override
+    public String toString() {
+        return "SocialHistoryBook{" +
+                "focusPopulation='" + focusPopulation + '\'' +
+                '}';
+    }
+}
