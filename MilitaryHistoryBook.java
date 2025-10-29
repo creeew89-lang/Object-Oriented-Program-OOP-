5e1da50f82c2bb29b5d0f89970d1c5189ab094b1
AL - Muiayed AL - Ansari <creeew89@gmail.com>
2025-10-29 14:54:51 +0400

    #TRA-5932: OOP > Shop > MilitaryHistoryBook
    A battlefield chronicle that zeroes in on a conflictFocus, dissecting campaigns through a battleAnalysisSection and startgicMaps that let readers march across turning points move by move.

---
+++ MilitaryHistoryBook.java
@@ -0,0 +1,14 @@
+package Vibe;
+
+public class MilitaryHistoryBook {
+    private String conflictFocus;
+    private boolean battleAnalysisSection;
+    private boolean startgicMaps;
+
+    @Override
+    public String toString() {
+        return "MilitaryHistoryBook{" +
+                "conflictFocus='" + conflictFocus + '\'' +
+                '}';
+    }
+}
