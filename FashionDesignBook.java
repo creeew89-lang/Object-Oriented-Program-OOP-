561abf961dcbb3eee3bdc167109d4ae554bf5d86
AL - Muiayed AL - Ansari <creeew89@gmail.com>
2025-10-29 14:20:52 +0400

    #TRA-5915: OOP > Shop > FashionDesignBook
    A style-stuffed DesignBook that drapes pages around a chosen gramentType, pinning fabricGuide swatches beside illustrationTechnique sketches to shepherd sketches from mood-board to runway-ready patterns.

---
+++ FashionDesignBook.java
@@ -0,0 +1,15 @@
+package Vibe;
+
+public class FashionDesignBook extends DesignBook{
+    private String gramentType;
+    private String fabricGuide;
+    private String illustrationTechnique;
+
+    @Override
+    public String toString() {
+        return "FashionDesignBook{" +
+                "gramentType='" + gramentType + '\'' +
+                '}';
+    }
+}
+
