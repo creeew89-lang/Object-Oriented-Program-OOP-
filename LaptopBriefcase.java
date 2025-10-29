e63f8e46e74597f0821c7b58d3980a450d142c73
AL - Muiayed AL - Ansari <creeew89@gmail.com>
2025-10-29 14:44:30 +0400

    #TRA-5927: OOP > Shop > LaptopBriefcase
    A LaptopBag tailored for boardrooms, its material shell hides numberOfCompartments for chargers, docs, and devices, all carried by a chosen strapType that swings from handles to shoulder.

---
+++ LaptopBriefcase.java
@@ -0,0 +1,14 @@
+package Vibe;
+
+public class LaptopBriefcase extends LaptopBag{
+    private int numberOfCompartments;
+    private String strapType;
+    private String material;
+
+    @Override
+    public String toString() {
+        return "LaptopBriefcase{" +
+                "numberOfCompartments=" + numberOfCompartments +
+                '}';
+    }
+}
