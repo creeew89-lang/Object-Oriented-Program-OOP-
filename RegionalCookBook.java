0a656f5100d59d156d99f5bc649828a2c1be8341
AL - Muiayed AL - Ansari <creeew89@gmail.com>
2025-10-29 15:09:52 +0400

    #TRA-5939: OOP > Shop > RegionalCookBook
    A culinary passport marking cuisineRegion, local cultrualContext, and ingredientSourcingGuide to plate authentic regional flavors.

---
+++ RegionalCookBook.java
@@ -0,0 +1,16 @@
+package Vibe;
+
+public class RegionalCookBook {
+    private String cuisineRegion;
+    private String cultrualContext;
+
+    @Override
+    public String toString() {
+        return "RegionalCookBook{" +
+                "cuisineRegion='" + cuisineRegion + '\'' +
+                '}';
+    }
+
+    private String ingredientSourcingGuide;
+
+}
