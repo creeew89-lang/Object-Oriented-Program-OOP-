e7d4a35b690d9ff8cbe2df9a5a9b642bc85e9cde
AL - Muiayed AL - Ansari <creeew89@gmail.com>
2025-11-03 09:15:11 +0400

    #TRA-5959: OOP > Hospital System > Bill

---
+++ Bill.java
@@ -0,0 +1,19 @@
+package Hospital.Models;
+
+public class Bill {
+    public Object getBillId() {
+        return null;
+    }
+
+    public void displayInfo() {
+
+    }
+
+    public String getPatientId() {
+        return null;
+    }
+
+    public String getBillClass() {
+        return null;
+    }
+}
