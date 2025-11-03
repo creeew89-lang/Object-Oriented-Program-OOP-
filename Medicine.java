07b7d654f959838d3b6c47c6b8bc5e4c38b54a5f
AL - Muiayed AL - Ansari <creeew89@gmail.com>
2025-11-03 09:24:54 +0400

    #TRA-5969: OOP > Hospital System > Medicine

---
+++ Medicine.java
@@ -0,0 +1,18 @@
+package Hospital.Models;
+
+public class Medicine {
+    public Object getMedicineId() {
+        return null;
+    }
+
+    public String getType() {
+        return null;
+    }
+
+    public String getExpiryDate() {
+        return null;
+    }
+
+    public void displayInfo() {
+    }
+}
