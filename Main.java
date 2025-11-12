2ad10c3d7365d9508b395fb9c78eeaf58daf21f5
AL - Muiayed AL - Ansari <creeew89@gmail.com>
2025-11-12 13:01:23 +0400

    #TRA-6062: OOP Practice: NetworkDefender > Main

--- Main.java
+++ Main.java
@@ -1,19 +1,8 @@
-package HeroVillainSimulator;
+package NetwrokDefender.OOP;
 
 public class Main {
-    public static void main(String[] args) {
-        Hero hero = new Hero("CyberKnight", "Hacking", 100);
-        Villain villain = new Villain("DarkPhantom", "Malware", 100);
-
-        System.out.println("Before battle:");
-        hero.displayStats();
-        villain.displayStats();
-
-        hero.attackVillain(villain);
-        villain.attackHero(hero);
-
-        System.out.println("\nAfter battle:");
-        hero.displayStats();
-        villain.displayStats();
+    public static void main(String[] args){
+        GameEngine gameEngine = new GameEngine();
+        gameEngine.startGame();
     }
 }
